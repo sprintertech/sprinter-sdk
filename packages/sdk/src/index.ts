933a9ec27bc79e73426eeeb461042c8317180f34
BeroBurny <bero4net@gmail.com>
2024-05-22 16:41:21 +0200

    basic implementation

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -1,28 +1,96 @@
-import { Chain, FungibleToken, Solution, SolutionOptions } from "./types";
+import {
+  getFungibleTokens,
+  getSolution,
+  getSupportedChains,
+  getUserFungibleTokens,
+  BASE_URL,
+} from "./api";
+import {
+  Address,
+  Chain,
+  FungibleToken,
+  FungibleTokenBalance,
+  Solution,
+  SolutionOptions,
+  TokenSymbol,
+} from "./types";
 
-export async function getSupportedChains(): Promise<Chain[]> {
-  return Promise.resolve([]);
+export type * from "./types";
+export * as api from "./api";
+
+// Note: code below is hacking, not a final api or approach
+// TODO: find correct type so all disables can be safely removed
+type EIP1193Provider = any;
+
+class GopherManager {
+  #provider: EIP1193Provider;
+
+  // local "cache"
+  #tokens: FungibleToken[] | undefined;
+  #chains: Chain[] | undefined;
+
+  constructor(provider: EIP1193Provider) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    this.#provider = provider;
+  }
+
+  public async getAvailableTokens(): Promise<FungibleToken[]> {
+    if (!this.#tokens) this.#tokens = await getFungibleTokens();
+    return this.#tokens;
+  }
+
+  public async getAvailableChains(): Promise<Chain[]> {
+    if (!this.#chains) this.#chains = await getSupportedChains();
+    return this.#chains;
+  }
+
+  public async getUserBalances(): Promise<{
+    [sybol: TokenSymbol]: FungibleTokenBalance[];
+  }> {
+    const account = await this.getAccount();
+
+    const tokenList = await this.getAvailableTokens();
+
+    const balances = await Promise.all(
+      tokenList.map((token) =>
+        getUserFungibleTokens(account, token.symbol).then((r) => ({
+          symbol: token.symbol,
+          ...r,
+        }))
+      )
+    );
+
+    return balances.reduce((previousValue, { symbol, ...currentValue }) => {
+      previousValue[symbol] = currentValue;
+      return previousValue;
+    }, {} as { [symbol: TokenSymbol]: FungibleTokenBalance[] });
+  }
+
+  public async getSolution(
+    settings: Omit<SolutionOptions, "account">
+  ): Promise<Solution[]> {
+    const account = await this.getAccount();
+
+    return await getSolution({ ...settings, account });
+  }
+
+  private async getAccount(): Promise<Address> {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access
+    const [account] = (await this.#provider.request({
+      method: "eth_requestAccounts",
+      params: [],
+    })) as unknown as Address[];
+    if (!account)
+      throw new Error("No available account! Check your provider or something");
+
+    return account;
+  }
 }
 
-export async function getKnownFungibleTokens(): Promise<FungibleToken[]> {
-  return Promise.resolve([]);
-}
-
-export async function getSolution({
-  account,
-  destinationChain,
-  token,
-  amount,
-  threshold,
-  whitelistedSourceChains,
-}: SolutionOptions): Promise<Solution[]> {
-  console.log(
-    account,
-    destinationChain,
-    token,
-    amount,
-    threshold,
-    whitelistedSourceChains
-  );
-  return Promise.resolve([]);
-}
+export {
+  getSupportedChains,
+  getFungibleTokens,
+  getSolution,
+  GopherManager,
+  BASE_URL,
+};
--- packages/sdk/src/types.ts
+++ packages/sdk/src/types.ts
@@ -1,13 +1,64 @@
-export interface Chain {}
-export interface FungibleToken {}
+export type Address = `0x${string}`;
 
-export interface Solution {}
+export type TokenSymbol = string;
+
+export type ChainID = number;
+
+export interface FungibleToken {
+  addresses: { [chainID: ChainID]: Address };
+  decimals: number;
+  logoURI: string;
+  name: string;
+  symbol: TokenSymbol;
+}
+
+export interface Chain {
+  chainID: ChainID;
+  chainType: "evm" /* string */;
+  name: string;
+  rpcurls: string[];
+}
+
+export interface FungibleTokenBalance {
+  balance: string /* big number as string*/;
+  chainId: ChainID;
+  tokenDecimals: number;
+}
 
 export interface SolutionOptions {
-  account: string;
-  destinationChain: Chain;
-  token: FungibleToken;
-  amount: bigint;
-  threshold?: bigint;
-  whitelistedSourceChains: Chain[];
+  account: Address;
+  destinationChain: ChainID;
+  token: TokenSymbol;
+  amount: number;
+  threshold?: number;
+  whitelistedSourceChains?: ChainID[];
+}
+
+interface Amount {
+  amount: string;
+  amountUSD: number;
+}
+
+export interface Solution {
+  destinationChain: ChainID;
+  destinationTokenAddress: Address;
+  duration: number /* estimation duration by seconds */;
+  fee: Amount;
+  gasCost: Amount;
+  senderAddress: Address;
+  sourceChain: ChainID;
+  sourceTokenAddress: Address;
+  tool: {
+    logoURI: string;
+    name: string;
+  };
+  transaction: {
+    chainId: ChainID;
+    data: string;
+    from: Address;
+    gasLimit: string;
+    gasPrice: string;
+    to: Address;
+    value: string;
+  };
 }
