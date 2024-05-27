e2fffa4478f2aedfb07cdb073e296f4675321c15
BeroBurny <bero4net@gmail.com>
2024-05-27 14:02:29 +0200

    submit TX action

--- web/src/lib/components/ResponseSendModal.svelte
+++ web/src/lib/components/ResponseSendModal.svelte
@@ -4,7 +4,9 @@
 	import { getModalStore } from '@skeletonlabs/skeleton';
 	import { hacks_getQuota } from '$lib/hacks';
 	import { formatDuration } from '$lib/formatters';
-	import { fromWei } from 'web3-utils';
+	import { fromWei, toHex } from 'web3-utils';
+	import { selectedProvider } from '$lib/stores/wallet';
+	import { Web3 } from 'web3';
 
 	// Props
 	/** Exposes parent props to this component. */
@@ -16,8 +18,43 @@
 	$: quota = hacks_getQuota($modalStore[0]?.value);
 	$: token = $modalStore[0]?.meta.tokens.get($modalStore[0]?.value.token);
 
-	async function submitTransaction(transaction: Object) {
-		console.log(transaction);
+	const submitting: boolean[] = [];
+	const successful: boolean[] = [];
+	// TODO: there is not place for this over here! refactor it to somewhere
+	async function submitTransaction(transaction: Object, index: number) {
+		try {
+			submitting[index] = true;
+
+			// Preparation /w questionable approach but will see for now
+			try {
+				await $selectedProvider.provider.request({
+					method: 'wallet_switchEthereumChain',
+					params: [{ chainId: toHex(transaction.chainId) }]
+				});
+			} catch (error) {
+				if (error.code === 4902) {
+					const network = $modalStore[0]?.meta.networks.get(transaction.chainId);
+					await $selectedProvider.provider.request({
+						method: 'wallet_addEthereumChain',
+						params: [
+							{
+								chainName: network.name,
+								chainId: toHex(transaction.chainId),
+								rpcUrls: network.rpcURLs
+							}
+						]
+					});
+				}
+			}
+
+			const web3 = new Web3($selectedProvider.provider);
+			const receipt = await web3.eth.sendTransaction(transaction);
+
+			console.warn(`TX receipt: `, receipt);
+			successful[index] = true;
+		} catch {
+			submitting[index] = false;
+		}
 	}
 
 	// Base Classes
@@ -34,7 +71,7 @@
 			{:then response}
 				<article class="container mx-auto p-4">
 					<ul class="space-y-4">
-						{#each response.data as data}
+						{#each response.data as data, index}
 							{@const network = $modalStore[0]?.meta.networks.get(data.sourceChain)}
 							{@const balance = $modalStore[0]?.meta.balances
 								.get(token.symbol)
@@ -55,15 +92,33 @@
 									</div>
 								</div>
 								<div class="flex flex-col items-center justify-center h-full">
-									<button
-										class="border border-blue-500 text-blue-500 text-xs px-2 py-1 rounded mb-1"
-										on:click={() => submitTransaction(data.transaction)}
-									>
-										Submit & Send
-									</button>
-									<p class="text-gray-400 text-xs">
-										Estimated Time {formatDuration(data.duration)}
-									</p>
+									{#if !successful[index]}
+										<button
+											class="border border-blue-500 text-blue-500 text-xs px-2 py-1 rounded mb-1"
+											disabled={submitting[index]}
+											on:click={() => submitTransaction(data.transaction, index)}
+										>
+											Submit & Send
+										</button>
+										<p class="text-gray-400 text-xs">
+											Estimated Time {formatDuration(data.duration)}
+										</p>
+									{:else}
+										<svg
+											class="w-6 h-6 text-green-500"
+											fill="none"
+											stroke="currentColor"
+											viewBox="0 0 24 24"
+											xmlns="http://www.w3.org/2000/svg"
+										>
+											<path
+												stroke-linecap="round"
+												stroke-linejoin="round"
+												stroke-width="2"
+												d="M5 13l4 4L19 7"
+											></path>
+										</svg>
+									{/if}
 								</div>
 							</li>
 						{/each}
