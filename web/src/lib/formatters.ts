d15ce0751ea5389a15e49b506ea885c3f01681ac
BeroBurny <bero4net@gmail.com>
2024-06-04 13:30:45 +0200

    smoll fixes

--- web/src/lib/formatters.ts
+++ web/src/lib/formatters.ts
@@ -1,5 +1,5 @@
 import { intlFormatDistance } from 'date-fns';
 
 export function formatDuration(value: number): string {
-	return intlFormatDistance(value / 10000, 0);
+	return intlFormatDistance(value * 1000, 0);
 }
