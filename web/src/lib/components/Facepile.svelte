9f00a262ec696d5b122797db04e20ee1bccbd4f1
BeroBurny <bero4net@gmail.com>
2024-06-06 16:18:12 +0200

    distribution icons

---
+++ web/src/lib/components/Facepile.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    export let balances;
+    export let networks;
+
+    $: list = balances.reduce((previousValue, { balance, chainId }) => {
+      if (balance != '0') {
+        const network = networks.get(chainId); // not having types yet! { name, logoURI }
+        previousValue.push({
+          balance: BigInt(balance),
+          chainId,
+          name: network.name,
+          logo: network.logoURI,
+        });
+      }
+      return previousValue;
+    }, []);
+</script>
+
+<div class="flex -space-x-2">
+    {#each list as item}
+        <img class="inline-block size-8 rounded-full ring-2 ring-white dark:ring-neutral-900" src={item.logo} alt={item.name} />
+    {/each}
+</div>
