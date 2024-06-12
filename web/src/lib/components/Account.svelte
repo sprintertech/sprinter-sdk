039e78499e620e20768d8f01a4384a4a017d7b45
BeroBurny <bero4net@gmail.com>
2024-06-12 12:17:26 +0200

    formatting

--- web/src/lib/components/Account.svelte
+++ web/src/lib/components/Account.svelte
@@ -26,7 +26,7 @@
 
 <div class="w-[1024px] h-full py-[15px] flex-col justify-start items-center gap-2.5 inline-flex">
 	<div
-			class="self-stretch h-[152px] px-[22px] py-[23px] bg-gray-200 dark:bg-gray-700 rounded-xl flex-col justify-start items-center gap-[34px] flex"
+		class="self-stretch h-[152px] px-[22px] py-[23px] bg-gray-200 dark:bg-gray-700 rounded-xl flex-col justify-start items-center gap-[34px] flex"
 	>
 		<div class="self-stretch justify-start items-start gap-2.5 inline-flex">
 			<div class="text-black dark:text-white text-xl font-medium font-['Inter'] leading-7">
@@ -41,11 +41,13 @@
 			</div>
 		</div>
 		<div class="self-stretch justify-end items-start gap-2 inline-flex">
-			<div class="p-2.5 bg-black dark:bg-gray-200 rounded-full justify-center items-center gap-2 flex">
+			<div
+				class="p-2.5 bg-black dark:bg-gray-200 rounded-full justify-center items-center gap-2 flex"
+			>
 				<button
-						type="button"
-						class="text-white dark:text-black text-base font-medium font-['Inter'] leading-normal"
-						on:click={openSendDrawer}
+					type="button"
+					class="text-white dark:text-black text-base font-medium font-['Inter'] leading-normal"
+					on:click={openSendDrawer}
 				>
 					Send
 				</button>
--- web/src/lib/components/SubmitTokensDrawer.svelte
+++ web/src/lib/components/SubmitTokensDrawer.svelte
@@ -79,26 +79,33 @@
 	}
 </script>
 
-<div class="self-stretch px-5 py-1.5 bg-slate-50 dark:bg-gray-600 justify-center items-center gap-1.5 inline-flex">
-	<div class="grow shrink basis-0 text-slate-400 dark:text-slate-200 text-xs font-medium font-['Inter'] uppercase leading-none tracking-wide">
+<div
+	class="self-stretch px-5 py-1.5 bg-slate-50 dark:bg-gray-600 justify-center items-center gap-1.5 inline-flex"
+>
+	<div
+		class="grow shrink basis-0 text-slate-400 dark:text-slate-200 text-xs font-medium font-['Inter'] uppercase leading-none tracking-wide"
+	>
 		Quotas
 	</div>
 </div>
 
-
-	<article class="container mx-auto p-4">
-		<ul class="space-y-4">
-			{#await quota}
-				<li class="bg-gray-200 dark:bg-gray-700 p-4 rounded-lg flex justify-between items-center mb-4">
-					<div class="placeholder h-[68px] w-full rounded-lg" />
-				</li>
-			{:then response}
+<article class="container mx-auto p-4">
+	<ul class="space-y-4">
+		{#await quota}
+			<li
+				class="bg-gray-200 dark:bg-gray-700 p-4 rounded-lg flex justify-between items-center mb-4"
+			>
+				<div class="placeholder h-[68px] w-full rounded-lg" />
+			</li>
+		{:then response}
 			{#each response.data as data, index}
 				{@const network = $drawerStore.meta.networks.get(data.sourceChain)}
 				{@const balance = $drawerStore.meta.balances
-						.get(token.symbol)
-						.find(({ chainId }) => chainId === data.sourceChain)}
-				<li class="bg-gray-200 dark:bg-gray-700 p-4 rounded-lg flex justify-between items-center mb-4">
+					.get(token.symbol)
+					.find(({ chainId }) => chainId === data.sourceChain)}
+				<li
+					class="bg-gray-200 dark:bg-gray-700 p-4 rounded-lg flex justify-between items-center mb-4"
+				>
 					<div class="flex items-center">
 						<img src={network.logoURI} alt="Source Chain Icon" class="w-8 h-8 mr-2" />
 						<div>
@@ -118,9 +125,9 @@
 					<div class="flex flex-col items-center justify-center h-full">
 						{#if !successful[index]}
 							<button
-									class="border border-blue-500 text-blue-500 text-xs px-2 py-1 rounded mb-1 dark:border-blue-300 dark:text-blue-300"
-									disabled={submitting[index]}
-									on:click={() => submitTransaction(data, index)}
+								class="border border-blue-500 text-blue-500 text-xs px-2 py-1 rounded mb-1 dark:border-blue-300 dark:text-blue-300"
+								disabled={submitting[index]}
+								on:click={() => submitTransaction(data, index)}
 							>
 								Submit & Send
 							</button>
@@ -129,39 +136,43 @@
 							</p>
 						{:else}
 							<svg
-									class="w-6 h-6 text-green-500 dark:text-green-300"
-									fill="none"
-									stroke="currentColor"
-									viewBox="0 0 24 24"
-									xmlns="http://www.w3.org/2000/svg"
+								class="w-6 h-6 text-green-500 dark:text-green-300"
+								fill="none"
+								stroke="currentColor"
+								viewBox="0 0 24 24"
+								xmlns="http://www.w3.org/2000/svg"
 							>
 								<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										stroke-width="2"
-										d="M5 13l4 4L19 7"
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									stroke-width="2"
+									d="M5 13l4 4L19 7"
 								></path>
 							</svg>
 						{/if}
 					</div>
 				</li>
 			{/each}
-			{:catch error}
-				<p style="color: red">{error.message}</p>
-			{/await}
-		</ul>
-	</article>
+		{:catch error}
+			<p style="color: red">{error.message}</p>
+		{/await}
+	</ul>
+</article>
 
-<div class="w-full p-5 bg-white dark:bg-gray-800 border-t border-zinc-200 dark:border-gray-600 items-center gap-4 inline-flex mt-auto">
+<div
+	class="w-full p-5 bg-white dark:bg-gray-800 border-t border-zinc-200 dark:border-gray-600 items-center gap-4 inline-flex mt-auto"
+>
 	<button
-			on:click={() => {
-      quota = hacks_getQuota($drawerStore.meta.quota);
-    }}
-			class="w-full h-10 p-2.5 bg-white dark:bg-gray-700 rounded-[10px] shadow border border-zinc-200 dark:border-gray-600 justify-center items-center gap-1 inline-flex"
+		on:click={() => {
+			quota = hacks_getQuota($drawerStore.meta.quota);
+		}}
+		class="w-full h-10 p-2.5 bg-white dark:bg-gray-700 rounded-[10px] shadow border border-zinc-200 dark:border-gray-600 justify-center items-center gap-1 inline-flex"
 	>
 		<div class="w-5 h-5 relative"></div>
 		<div class="px-1 justify-center items-center flex">
-			<div class="text-gray-600 dark:text-gray-200 text-sm font-medium font-['Inter'] leading-tight">
+			<div
+				class="text-gray-600 dark:text-gray-200 text-sm font-medium font-['Inter'] leading-tight"
+			>
 				Get another Quota
 			</div>
 		</div>
