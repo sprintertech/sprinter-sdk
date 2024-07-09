d8f52acb5fc93aa995688fe13ea13d2ef8510df1
BeroBurny <bero4net@gmail.com>
2024-07-09 15:22:16 +0200

    lint fix

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -5,13 +5,8 @@
 <AppBar class="bg-white dark:bg-gray-800">
 	<svelte:fragment slot="lead">
 		<div class="h-10 px-2.5 justify-start items-center gap-2.5 flex">
-			<div
-				class="w-10 h-10 relative bg-slate-200 dark:bg-slate-700 overflow-hidden bg-transparent"
-			>
-				<img
-					src="https://s12.gifyu.com/images/StEyL.gif"
-					alt="Joke Logo"
-				/>
+			<div class="w-10 h-10 relative bg-slate-200 dark:bg-slate-700 overflow-hidden bg-transparent">
+				<img src="https://s12.gifyu.com/images/StEyL.gif" alt="Joke Logo" />
 			</div>
 			<div class="text-black dark:text-white text-xl font-medium font-['Inter'] leading-7">
 				Sprinter Demo
