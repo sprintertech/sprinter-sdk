830c2ca98af7d051c373972c0402f85f62532307
BeroBurny <bero4net@gmail.com>
2024-07-30 11:36:23 +0200

    feat: sentry

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,17 @@
+import { sentrySvelteKit } from '@sentry/sveltekit';
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), purgeCss()]
+	plugins: [
+		sentrySvelteKit({
+			sourceMapsUploadOptions: {
+				org: 'chainsafe-hy',
+				project: 'typescript-sveltekit'
+			}
+		}),
+		sveltekit(),
+		purgeCss()
+	]
 });
