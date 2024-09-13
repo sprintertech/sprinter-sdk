af88bff16c094432f08785d7d3af97cd31d4df80
BeroBurny <bero4net@gmail.com>
2024-09-13 13:41:30 +0200

    icon and theme

--- web/src/lib/components/Header.svelte
+++ web/src/lib/components/Header.svelte
@@ -1,12 +1,14 @@
 <script>
-	import { AppBar, LightSwitch } from '@skeletonlabs/skeleton';
+	import { AppBar, LightSwitch, modeCurrent } from '@skeletonlabs/skeleton';
+	import logoDark from '$lib/assets/sprinter-logo.svg';
+	import logoLight from '$lib/assets/sprinter-logo-white.svg';
 </script>
 
 <AppBar class="bg-white dark:bg-gray-800">
 	<svelte:fragment slot="lead">
 		<div class="h-10 px-2.5 justify-start items-center gap-2.5 flex">
 			<div class="w-10 h-10 relative bg-slate-200 dark:bg-slate-700 overflow-hidden bg-transparent">
-				<img src="https://s12.gifyu.com/images/StEyL.gif" alt="Joke Logo" />
+				<img src={$modeCurrent ? logoDark : logoLight} alt="SprinterLogo" />
 			</div>
 			<div class="text-black dark:text-white text-xl font-medium font-['Inter'] leading-7">
 				Sprinter Demo
