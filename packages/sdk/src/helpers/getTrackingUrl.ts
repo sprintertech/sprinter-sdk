419b23b899d86dc3611fa43c9020fbe356ab313e
BeroBurny <bero4net@gmail.com>
2024-09-23 10:21:04 +0200

    resolve abi comment

--- packages/sdk/src/helpers/getTrackingUrl.ts
+++ packages/sdk/src/helpers/getTrackingUrl.ts
@@ -1,6 +1,7 @@
 import { createPublicClient, http, parseEventLogs } from "viem";
 
 import type { Address, Chain, Tool } from "../types";
+import { acrossV3Abi } from "./acrossV3.abi";
 
 export async function experimental_getTrackingUrl(
   transactionHash: string,
@@ -56,91 +57,3 @@ export async function experimental_getTrackingUrl(
       return "Tool not recognized.";
   }
 }
-
-const acrossV3Abi = [
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: false,
-        internalType: "address",
-        name: "inputToken",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "address",
-        name: "outputToken",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "inputAmount",
-        type: "uint256",
-      },
-      {
-        indexed: false,
-        internalType: "uint256",
-        name: "outputAmount",
-        type: "uint256",
-      },
-      {
-        indexed: true,
-        internalType: "uint256",
-        name: "destinationChainId",
-        type: "uint256",
-      },
-      {
-        indexed: true,
-        internalType: "uint32",
-        name: "depositId",
-        type: "uint32",
-      },
-      {
-        indexed: false,
-        internalType: "uint32",
-        name: "quoteTimestamp",
-        type: "uint32",
-      },
-      {
-        indexed: false,
-        internalType: "uint32",
-        name: "fillDeadline",
-        type: "uint32",
-      },
-      {
-        indexed: false,
-        internalType: "uint32",
-        name: "exclusivityDeadline",
-        type: "uint32",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "depositor",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "address",
-        name: "recipient",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "address",
-        name: "exclusiveRelayer",
-        type: "address",
-      },
-      {
-        indexed: false,
-        internalType: "bytes",
-        name: "message",
-        type: "bytes",
-      },
-    ],
-    name: "V3FundsDeposited",
-    type: "event",
-  },
-] as const;
