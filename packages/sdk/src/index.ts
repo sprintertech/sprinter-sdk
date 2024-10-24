4d524c668393f33c7c200a81f75cab87a3114e9d
Bernard Stojanović <bero4net@gmail.com>
2024-10-24 17:47:16 +0200

    feat: get bridge history for an address (#68)
    # Summary
    Added method to fetch bridge transactions history of an address
    
    # Closes: #67 
    
    # Description
    The method should return aggregated data from all bridges that are
    supported by Sprinter API. Right now, only Sygma is added but later
    other bridge will be added
    
    # Chores:
    - [ ] Unit tests
    - [x] TS Docs / Comments
    
    ---------
    
    Co-authored-by: Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -3,3 +3,4 @@ export { setBaseUrl, BASE_URL } from "./api";
 export * as api from "./api";
 export { ChainType, Environment } from "./enums";
 export { Sprinter } from "./sprinter";
+export * from "./helpers";
