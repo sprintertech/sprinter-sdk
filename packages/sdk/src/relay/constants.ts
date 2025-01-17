06ca1a0d8b5cb76bc06cd9aeaf58ac81b197cde3
Saad Ahmed <48211799+saadahmsiddiqui@users.noreply.github.com>
2025-01-17 16:42:08 +0100

    feat: expand bridge transaction history using relay API (#122)
    Closes: #120 
    
    Added method that returns transactions using relay protocol given an
    address
    
    ---------
    
    Co-authored-by: BeroBurny <bero4net@gmail.com>

---
+++ packages/sdk/src/relay/constants.ts
@@ -0,0 +1,18 @@
+import { Environment } from "../enums";
+import { Status } from "../sygma";
+
+import { RelayRequestStatus } from "./types";
+
+export const RELAY_API_ENDPOINTS: Record<Environment, string> = {
+  [Environment.MAINNET]: "https://api.relay.link",
+  [Environment.TESTNET]: "https://api.testnets.relay.link",
+};
+
+export const RELAY_STATUSES_MAPPING = new Map<RelayRequestStatus, Status>([
+  [RelayRequestStatus.Refund, Status.failed],
+  [RelayRequestStatus.Delayed, Status.pending],
+  [RelayRequestStatus.Waiting, Status.pending],
+  [RelayRequestStatus.Failure, Status.failed],
+  [RelayRequestStatus.Pending, Status.pending],
+  [RelayRequestStatus.Success, Status.executed],
+]);
