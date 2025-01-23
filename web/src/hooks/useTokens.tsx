4c4bb2acfb800d8a0a716aa8fe99ed3a8c8528da
Thibaut Sardan <github@thib.top>
2025-01-23 16:18:13 +0100

    ETH in a const

--- web/src/hooks/useTokens.tsx
+++ web/src/hooks/useTokens.tsx
@@ -1,19 +1,22 @@
 import { useSprinterTokens } from '@chainsafe/sprinter-react'
 import { useMemo } from 'react'
 
+// Nt provided by Sprinter in the useSprinterTokens
+const ETH = {
+  name: 'ethereum',
+  decimals: 18,
+  symbol: 'ETH',
+  logoURI: 'https://scan.buildwithsygma.com/assets/icons/evm.svg',
+  addresses: []
+}
+
 export const useTokens = () => {
   const {
     tokens: { data, error, loading }
   } = useSprinterTokens()
 
   const tokens = useMemo(() => {
-    return (data ?? []).concat({
-      name: 'ethereum',
-      decimals: 18,
-      symbol: 'ETH',
-      logoURI: 'https://scan.buildwithsygma.com/assets/icons/evm.svg',
-      addresses: []
-    })
+    return (data ?? []).concat(ETH)
   }, [data])
 
   return {
