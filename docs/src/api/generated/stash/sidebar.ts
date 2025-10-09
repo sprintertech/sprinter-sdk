1583e74b750fd4b5a7fe1b5887f742221ea3061f
Bernard Stojanović <bero4net@gmail.com>
2025-10-09 07:33:46 +0200

    fix: borrow quote example (#189)

--- docs/src/api/generated/stash/sidebar.ts
+++ docs/src/api/generated/stash/sidebar.ts
@@ -8,15 +8,15 @@ const sidebar: SidebarsConfig = {
       items: [
         {
           type: "doc",
-          id: "../src/api/generated/stash/get-the-borrow-cost-for-a-liquidity-transaction",
-          label: "Get the borrow cost for a liquidity transaction",
+          id: "../src/api/generated/stash/get-signing-authorization-for-a-liquidity-transaction",
+          label: "Get signing authorization for a liquidity transaction",
           className: "api-method get",
         },
         {
           type: "doc",
           id: "../src/api/generated/stash/get-the-borrow-quote-for-a-liquidity-transaction-based-on-the-input-data",
           label:
-            "Get the borrow quote for a liquidity transaction based on the input data",
+            "Get the borrow quote for a liquidity transaction based on the input data.",
           className: "api-method get",
         },
       ],
